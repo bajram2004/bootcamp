3d483144a7d6f6c7f7926fb810992264b5625835
bajram2004 <99073421@mydavinci.nl>
2023-09-07 08:22:03 +0200

    les_X_opdX.py

---
+++ Deel_1/Les_3/uitwerkingen/les_X_opdX.py/Opdracht 1.py
@@ -0,0 +1,13 @@
+appels = 3.40
+druiven = 2.45
+bananen = 1.95
+BTW = 9
+appelsbtw = (appels / 100 * BTW)
+appelstotaal = appels + appelsbtw
+druivenbtw = (druiven / 100 * BTW)
+druiventotaal = druiven + druivenbtw
+bananenbtw = (bananen / 100 * BTW)
+bananentotaal = bananen + bananenbtw
+print(appelstotaal)
+print(druiventotaal)
+print(bananentotaal) 
