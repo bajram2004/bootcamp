35ef4773e56c5a93944f1bd49ecc108b218f56a3
bajram2004 <99073421@mydavinci.nl>
2023-11-06 09:59:19 +0100

    upd

---
+++ Deel_2/Les_12/les_12.py
@@ -0,0 +1,15 @@
+#opdeacht 1
+name = input("wat is je naam ")
+age = int(input("hoe oud ben je "))
+leeftijd = age + 5
+print ("hallo "+ name +" je bent over 5 jaar " + str(leeftijd) +" jaar oud.")
+
+#opdracht 2
+g1 = float(input ("kies een getal "))
+g2 = float(input ("kies nog een getal "))
+gem = (g1 + g2) / 2
+print ("het gemiddelde is "+ str(gem))
+
+#opdracht 3
+woord = input ("voer een woord in ")
+print ("Het woord is: "+ woord +" en bestaat uit " +str(len(woord))+ " tekens.")
