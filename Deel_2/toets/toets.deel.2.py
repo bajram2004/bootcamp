6e293d730574c9561e491de2179deed1d20f529b
bajram2004 <99073421@mydavinci.nl>
2023-11-09 13:19:31 +0100

    upd

--- Deel_2/toets/toets.deel.2.py
+++ Deel_2/toets/toets.deel.2.py
@@ -1,6 +1,6 @@
 # opdracht 1
-#
-#
+#A vs heeft extensies en een ingebouwde debugger, ook laat vs als je een code invoert zien wat je nog meer kan 
+#B het is goed als je samen werkt en als je een groote code scrijft en je hebt een fout kan je altijd nog terug naar een ander versie 
 
 # opdracht 2
 
@@ -49,4 +49,40 @@ else:
     print("Niet aan de hand, gebruik je mobiel lekker verder!")
 
 # opdracht 7
- 
+print("\n".join(map(str, range(1, 251)))) 
+
+# opdract 8
+lijst_eten = ['appel', 'pannenkoek', 'kiwi', 'hamburger']
+
+#A
+print("Onze menukaart:")
+for gerecht in lijst_eten:
+    print(gerecht)
+
+#B
+langste_eten = max(lijst_eten, key=len)
+print(langste_eten)
+
+# opdracht 9
+
+while True:
+    try:
+        cijfer = float(input("Voer een cijfer tussen 0 en 10 in: "))
+        if 0 <= cijfer <= 10:
+            break  # Stop de lus als het cijfer geldig is
+        else:
+            print("Fout: Voer een cijfer tussen 0 en 10 in.")
+    except ValueError:
+        print("Fout: Voer een geldig cijfer in.")
+
+# opdracht 10
+
+MAX = 20
+getal = int(input("Voer een getal in"))
+
+if getal > MAX:
+    input(f"Het getal is groter dan {MAX}")
+elif getal < MAX:
+    input(f"Het getal is kleiner dan {MAX}")
+else:
+    input(f"Het getal is gelijk aan {MAX}")
