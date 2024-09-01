6aa9290db82d5fa4bf5232decfd3045bfa669ece
bajram2004 <99073421@mydavinci.nl>
2024-09-01 14:15:53 +0200

    upd

---
+++ deel_1.1/bc_l2_opdracht2.py
@@ -0,0 +1,7 @@
+print(15+4)#optellen
+print(15-4)#aftrekken
+print(15*4)#vermenigvuldigen
+print(15/4)#delen
+print(15//4)#gehele deling
+print(15**4)#machtsverheffing
+print(15%4)#restant deling
---
+++ deel_1.1/bc_l2_opdracht3.py
@@ -0,0 +1,16 @@
+# We verwachten: 199.5126050420168
+print (((12*13 /14) + (15 -16) /17) *18)
+
+# Maak met de volgende reeks 5 verschillende uitkomsten,
+# door haakjes op andere plekken te zetten
+print( 2*3 /4 + 5 -6/7 *-1)
+#1
+print((2*3) /4 + 5 -6/7 *-1)
+#2
+print(2*(3 /4) + 5 -6/7 *-1)
+#3
+print(2*3 /(4 + 5) -6/7 *-1)
+#4
+print(2*3 / (4 + 5 - 6/7) *-1)
+#5
+print(2*3 /4 + (5 -6/7) *-1)
