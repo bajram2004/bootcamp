e7b4cb4e7de2146a6e07cce765531cb74d5d8ed1
bajram2004 <99073421@mydavinci.nl>
2024-09-05 13:40:57 +0200

    .

--- deel_1.1/les5/bc_l4_opdracht5.py
+++ deel_1.1/les5/bc_l4_opdracht5.py
@@ -7,10 +7,10 @@ for _ in range(20):
     print("Ik ben hard op weg developer te worden!")
 
 #opdracht 3
-total = 625
-divisor = 25
-count = 0
-while total >= divisor:
-    total -= divisor
-    count += 1
-print(count)
+totaal = 625
+deel = 25
+ant = 0
+while totaal >= deel:
+    totaal -= deel
+    ant += 1
+print(ant)
